d28382da66ba819d51b6b4a37613fa863bf72c09
SushiFu <SushiFu@gmx.fr>
2017-04-29 19:53:13 +1000

    :fire: Remove useless example

--- src/commands/all.js
+++ src/commands/all.js
@@ -1,2 +1,1 @@
-import "./ping";
 import "./twitter";
--- src/commands/ping.js
+++
@@ -1,7 +0,0 @@
-import discord from "../server/discord";
-
-discord.onMessage(message => {
-    if (message.content === "!ping") {
-        discord.sendDefault("pong");
-    }
-});
