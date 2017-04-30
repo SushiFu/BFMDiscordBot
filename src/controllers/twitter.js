c9bb7822204151d880a37b1b19c95f67a3d9bc88
SushiFu <SushiFu@gmx.fr>
2017-04-30 21:24:53 +1000

    :bug: Improve tweets filter: only tweets from registered account & RT message

--- src/controllers/twitter.js
+++ src/controllers/twitter.js
@@ -1,6 +1,8 @@
 import twitter from "../server/twitter";
 import TwitterUser from "../models/twitter-user";
 
+import logger from "../server/logger";
+
 /**
  * Get twitter user id by usertag
  * @param {String} usertag
@@ -87,12 +89,25 @@ function reloadStream(callback) {
         .then(users => users.map(user => user.userId))
         .then(ids => {
             stream = twitter.stream("statuses/filter", { follow: ids });
-            stream.on("tweet", tweet => {
-                if (!tweet.retweeted_status) {
-                    callback(tweet);
-                }
-            });
+            stream.on("tweet", tweet => filterTweet(tweet, callback));
         });
+}
+
+/**
+ * Filter tweet
+ * @param {Object} tweet
+ * @param {Function} callback 
+ */
+function filterTweet(tweet, callback) {
+    logger.debug("Receive Tweet from:", tweet.user.screen_name);
+    listTwitterUsers()
+        .then(users => {
+            let ids = users.map(user => user.userId);
+            if (ids.includes(tweet.user.id_str)) {
+                callback(tweet);
+            }
+        })
+        .catch(err => logger.error(err));
 }
 
 export default { saveTwitterUsers, listTwitterUsers, deleteTwitterUsers, reloadStream };
