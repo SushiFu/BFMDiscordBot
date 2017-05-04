9591cd20d02e2444c16c8488938cf5344036c4e9
SushiFu <SushiFu@gmx.fr>
2017-05-05 00:13:06 +1000

    :bug: Get & list definitions on all channels

--- src/commands/wordDefinition.js
+++ src/commands/wordDefinition.js
@@ -13,6 +13,51 @@ const commands = {
   get: "help",
 };
 
+discord.onMessageEverywhere(message => {
+  if (message.content.startsWith("!help")) {
+    const extract = /!(.\S+)\s*([\s\S]*)/m.exec(message.content);
+    const command = extract[1];
+    const params = extract.length > 1 ? extract[2] : "";
+    if (command === commands.get) {
+      getWordDefinition(message.channel, params);
+    }
+    else if (command === commands.list) {
+      listWordDefinition(message.channel);
+    }
+  }
+});
+
+function getWordDefinition(channel, word) {
+  wordDefinitionCtrl.getWordDefinition(word)
+    .then(wordDefinition => {
+      if (wordDefinition) {
+        logger.info(`Definition for ${wordDefinition.word} sent:  ${wordDefinition.definition}`);
+        channel.send(wordDefinition.definition);
+      }
+      else {
+        channel.send(`Can't find a definition for the word ${word}`);
+      }
+    })
+    .catch((err) => {
+      logger.error(err);
+    });
+}
+
+function listWordDefinition(channel) {
+  wordDefinitionCtrl.listWordDefinition()
+    .then(wordDefinitions => {
+      const definitions = wordDefinitions.map((wordDefinition) => {
+        return wordDefinition.word;
+      });
+      logger.info(definitions);
+      channel.send(definitions);
+    })
+    .catch((err) => {
+      logger.error(err);
+      discord.sendCmdDefault("Ho shit, something goes wrong");
+    });
+}
+
 /**
  * Extract command and params, and peform corresponding action.
  */
@@ -29,31 +74,25 @@ discord.onMessage(message => {
     else if (command === commands.delete) {
       deleteWordDefinition(params);
     }
-    else if (command === commands.list) {
-      listWordDefinition();
-    }
-    else if (command === commands.get) {
-      getWordDefinition(params);
-    }
   }
 });
 
 function addWordDefinition(message) {
   // match word :: definition
   const extract = /(.*)\s+::\s+(.*)/m.exec(message);
-  if(!isNil(extract) && extract.length === 3) {
+  if (!isNil(extract) && extract.length === 3) {
     const word = extract[1];
     const definition = extract[2];
     wordDefinitionCtrl.saveWordDefinition(word, definition)
-    .then(wordDefinition => {
-      const log = `Definition for ${wordDefinition.word} has been added: \n${wordDefinition.definition}`;
-      logger.info(log);
-      discord.sendCmdDefault(log);
-    })
-    .catch((err) => {
-      logger.error(err);
-      discord.sendCmdDefault(`The word ${word} already exists`);
-    });
+      .then(wordDefinition => {
+        const log = `Definition for ${wordDefinition.word} has been added: \n${wordDefinition.definition}`;
+        logger.info(log);
+        discord.sendCmdDefault(log);
+      })
+      .catch((err) => {
+        logger.error(err);
+        discord.sendCmdDefault(`The word ${word} already exists`);
+      });
   }
   else {
     const log = "Can't add the definition. use the format word :: definition.";
@@ -62,42 +101,15 @@ function addWordDefinition(message) {
   }
 }
 
-function getWordDefinition(word) {
-  wordDefinitionCtrl.getWordDefinition(word)
-  .then(wordDefinition => {
-    logger.info(`Definition for ${wordDefinition.word} sent:  ${wordDefinition.definition}`);
-    discord.sendCmdDefault(wordDefinition.definition);
-  })
-  .catch((err) => {
-    logger.error(err);
-    discord.sendCmdDefault(`Can't find a definition for the word ${word}`);
-  });
-}
-
 function deleteWordDefinition(word) {
   wordDefinitionCtrl.deleteWordDefinition(word)
-  .then(word => {
-    const log = `Definition ${word} has been deleted`;
-    logger.info(log);
-    discord.sendCmdDefault(log);
-  })
-  .catch((err) => {
-    logger.error(err);
-    discord.sendCmdDefault(`Can't find and delete the word ${word}`);
-  });
-}
-
-function listWordDefinition() {
-  wordDefinitionCtrl.listWordDefinition()
-  .then(wordDefinitions => {
-    const definitions = wordDefinitions.map((wordDefinition) => {
-      return wordDefinition.word;
+    .then(word => {
+      const log = `Definition ${word} has been deleted`;
+      logger.info(log);
+      discord.sendCmdDefault(log);
+    })
+    .catch((err) => {
+      logger.error(err);
+      discord.sendCmdDefault(`Can't find and delete the word ${word}`);
     });
-    logger.info(definitions);
-    discord.sendCmdDefault(definitions);
-  })
-  .catch((err) => {
-    logger.error(err);
-    discord.sendCmdDefault("Ho shit, something goes wrong");
-  });
 }
